c416618fa515fd8bb2b18b55e75cde57a5fd8713
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-11-07 13:58:31 +0530

    The UI is built ✅

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,6 +2,8 @@ import Button from "@mui/material/Button";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TasksContext } from "../../contexts/TasksContext";
+import TaskCard from "../TaskCard";
+import { Task } from "../../types";
 
 export default function Board() {
   const navigate = useNavigate();
@@ -23,6 +25,21 @@ export default function Board() {
         <Button onClick={handleOnNewTask} variant="contained">
           New Task
         </Button>
+      </div>
+
+      <div className="board">
+        {Object.keys(board)?.map((item: any) => {
+          return (
+            <div>
+              <div className="board-header">{item}</div>
+              <div className="board-section">
+                {board[item]?.map((task: Task) => {
+                  return <TaskCard name={task?.name} status={task?.status} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
